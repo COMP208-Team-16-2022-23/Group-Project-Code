5b2c30ddc3f2e6bb87cfd2d32a8c6408c466263e
Dubi253 <dubi253@outlook.com>
2023-02-25 17:23:01 +0000

    feat: add test flask cgi

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,8 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from flask import Flask
+
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
