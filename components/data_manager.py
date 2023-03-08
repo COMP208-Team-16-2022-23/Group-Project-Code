0da5741a18ccb2e4b8fcd4c2102dd7d2fd1def4c
J_LJQ <36041971+Jason-LJQ@users.noreply.github.com>
2023-03-08 17:20:53 +0000

    improve footer

--- components/data_manager.py
+++ components/data_manager.py
@@ -6,6 +6,7 @@
 from flask import Blueprint, request, render_template, session, redirect, g, flash, url_for
 from werkzeug.utils import secure_filename
 import os
+import requests
 
 bp = Blueprint('my_data', __name__, template_folder='templates')
 
@@ -42,3 +43,17 @@ def mydata():
             dict_files = os.listdir('temp_files')
             return render_template('dataset/my_data.html', list=dict_files)
     return render_template('dataset/my_data.html', list=dict_files)
+
+@bp.route("/view_document", methods=['GET'])
+def view_document():
+    # Replace the URL with the URL of your Office document
+    document_url = 'https://binaries.templates.cdn.office.net/support/templates/zh-cn/tf55871247_win32.dotx'
+    # Replace the 'Office Online' string with your desired title for the viewer
+    title = 'Office Online'
+    # Build the HTML code for the viewer
+    viewer_html = requests.get(f'https://view.officeapps.live.com/op/embed.aspx?src={document_url}').text
+    return render_template('dataset/document_viewer.html', title=title, viewer_html=viewer_html)
+
+
+
+
