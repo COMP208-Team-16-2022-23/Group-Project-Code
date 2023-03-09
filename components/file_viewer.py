878ff41dfbb1a9aab5e47a51bf3fa2c2a22e9380
J_LJQ <36041971+Jason-LJQ@users.noreply.github.com>
2023-03-09 13:27:21 +0000

    [fix] download security check to avoid data leckage.

--- components/file_viewer.py
+++ components/file_viewer.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from flask import send_file
 import requests
+import os
+
 
 bp = Blueprint('file_viewer', __name__, template_folder='templates')
 
@@ -22,19 +24,27 @@ def view_document_demo():
 @bp.route('/view_document/<path:file_path>', methods=['GET'])
 def view_document(file_path='https://binaries.templates.cdn.office.net/support/templates/zh-cn/tf55871247_win32.dotx'):
     # Replace the URL with the URL of your Office document
-    document_url = f'https://lcda-vgnazlwvxa-nw.a.run.app/download_csv/{file_path}'
-    # document_url = 'https://lcda-vgnazlwvxa-nw.a.run.app/download_csv/temp_files/helloWorld.csv'
+    document_url = f'https://lcda-vgnazlwvxa-nw.a.run.app/download_file/{file_path}'
+    # document_url = 'https://lcda-vgnazlwvxa-nw.a.run.app/download_file/temp_files/helloWorld.csv'
+    from app import app
+    document_path = os.path.join(app.root_path, file_path)
     # Replace the 'Office Online' string with your desired title for the viewer
     title = 'Document Viewer'
     # Build the HTML code for the viewer
-    viewer_html = requests.get(f'https://view.officeapps.live.com/op/embed.aspx?src={document_url}').text
+    viewer_html = requests.get(f'https://view.officeapps.live.com/op/embed.aspx?src=file://{document_path}').text
     return render_template('dataset/document_viewer.html', title=title, viewer_html=viewer_html)
 
 
 ## download file
-@bp.route('/download_csv/<path:file_path>')
-def download_file(file_path='temp_files/helloWorld.csv'):
+@bp.route('/download_file/<path:file_path>')
+def download_file(file_path='sandbox/sample_user/hello_world.csv'):
     # Specify the file path
+    
+    # safety check
+    #if file_path do not contain 'sandbox', return 404
+    if 'sandbox' not in file_path:
+        return '404'
+    
     filename = file_path.split('/')[-1]
     # Send the file to the client
     return send_file(file_path, as_attachment=True, download_name=filename)
