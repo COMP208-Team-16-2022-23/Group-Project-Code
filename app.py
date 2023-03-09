4af9aaf18609a765818207a939df32a1715dbfe3
J_LJQ <36041971+Jason-LJQ@users.noreply.github.com>
2023-03-09 22:39:15 +0000

    small fix

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def shutdown_session(exception=None):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
