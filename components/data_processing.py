8a6e62fbd581aa13290296a07bf7f4e8e840cf21
Dubi253 <dubi253@outlook.com>
2023-03-14 22:16:55 +0000

    feat: create a new database table to store processing  project
    Add logic to redirect to the project editor page.
    Update yml file.
    Rename data_processing.py index page function name from 'data_processing' to 'index' and update relevant url in base.html

--- components/data_processing.py
+++ components/data_processing.py
@@ -2,6 +2,10 @@ from flask import Blueprint, request, render_template, session, redirect, g, url
 
 from util.storage_control import list_blobs
 
+# database import
+from database import db_session
+from util.models import ProcessingProject, User
+
 bp = Blueprint('data_processing', __name__, url_prefix='/data_processing')
 
 
@@ -11,12 +15,12 @@ def get_date_and_id(prefix):
     for blob in blobs_list:
         file_list.append(
             {'file_name': blob.name.replace(prefix + '/', ""), 'date_modified': str(blob.updated).split('.')[0],
-             'ID': str(blob.id).split('/')[-1]})
+             'id': str(blob.id).split('/')[-1]})
     return file_list
 
 
 @bp.route("/", methods=['GET', 'POST'])
-def data_processing():
+def index():
     # todo: add logic for data processing project list and add new project button
     prefix = 'public'
     file_list = get_date_and_id(prefix)
@@ -24,14 +28,53 @@ def data_processing():
         prefix = g.user.username
         file_list += get_date_and_id(prefix)
 
+    # get user's processing project list from database
+    if g.user:
+        processing_project_list = ProcessingProject.query.filter_by(user_id=g.user.id).all()
+    else:
+        processing_project_list = ProcessingProject.query.filter_by(user_id=User.query.filter(
+            User.username == 'public').first().id).all()
+
+    # logic for add new processing project
     if request.method == 'POST':
         selected_file_id = request.form['file_selection']
-        return redirect(url_for('data_processing.data_processing_project', project_id=selected_file_id))
 
-    return render_template('data_processing/index.html', file_list=file_list)
+        # check whether the file is already in processing, if so, redirect to the processing project page
+        for processing_project in processing_project_list:
+            if processing_project.original_file_id == selected_file_id:
+                return redirect(url_for('data_processing.project',
+                                        processing_project_id=processing_project.id))
+
+        # add new processing project to database
+        # get file name from file id
+        file_name = ''
+        for file in file_list:
+            if file['id'] == selected_file_id:
+                file_name = file['file_name']
+                break
+
+        # determine whether log in
+        if not g.user:
+            user_id = User.query.filter(User.username == 'public').first().id
+        else:
+            user_id = g.user.id
+
+        processing_project = ProcessingProject(user_id=user_id, original_file_id=selected_file_id,
+                                               original_file_name=file_name)
+        db_session.add(processing_project)
+        db_session.commit()
+
+        # get the latest id of the new processing project
+        processing_project_id = ProcessingProject.query.filter_by(user_id=user_id, original_file_id=selected_file_id,
+                                                                  original_file_name=file_name).first().id
+
+        return redirect(url_for('data_processing.project', processing_project_id=processing_project_id))
+
+    return render_template('data_processing/index.html', file_list=file_list,
+                           processing_project_list=processing_project_list)
 
 
-# todo: add route for data processing project, e.g. /data_processing/project/1234567 (project id)
-@bp.route("/project/<project_id>", methods=['GET', 'POST'])
-def data_processing_project(project_id):
-    return render_template('data_processing/project.html', project_id=project_id)
+@bp.route("/project/<processing_project_id>", methods=['GET', 'POST'])
+def project(processing_project_id):
+    # todo: restrict access to the project page if the user is not the owner of the project
+    return render_template('data_processing/project.html', project_id=processing_project_id)
--- util/models.py
+++ util/models.py
@@ -15,7 +15,6 @@ class User(Base):
     last_password_change_time = Column(DateTime, default=datetime.utcnow)
     last_login = Column(DateTime, unique=False, nullable=True)
 
-
     def __init__(self, email=None, username=None, password=None, password_reminder=None):
         self.email = email
         self.username = username
@@ -24,3 +23,40 @@ class User(Base):
 
     def __repr__(self):
         return {'id': self.id, 'email': self.email, 'username': self.username, 'password': self.password, }
+
+
+class ProcessingProject(Base):
+    """
+    This class is used to store the information of the processing project.
+
+    Attributes:
+        id: the id of the processing project
+        user_id: the id of the user who created the processing project
+        original_file_id: the id of the original file
+        original_file_name: the name of the original file
+        current_file_id: the id of the current file
+        current_file_name: the name of the current file
+        modified_date: the date when the processing project is modified
+    """
+
+    __tablename__ = 'processing_projects'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, nullable=False)
+    original_file_id = Column(String(500), nullable=False)
+    original_file_name = Column(String(500), nullable=False)
+    current_file_id = Column(String(500), nullable=False)
+    current_file_name = Column(String(500), nullable=False)
+    modified_date = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, user_id=None, original_file_id=None, original_file_name=None, current_file_id=original_file_id,
+                 current_file_name=original_file_name):
+        self.user_id = user_id
+        self.original_file_id = original_file_id
+        self.original_file_name = original_file_name
+        self.current_file_id = current_file_id
+        self.current_file_name = current_file_name
+
+    def __repr__(self):
+        return {'id': self.id, 'user_id': self.user_id, 'original_file_id': self.original_file_id,
+                'original_file_name': self.original_file_name, 'current_file_id': self.current_file_id,
+                'current_file_name': self.current_file_name, 'modified_date': self.modified_date}
