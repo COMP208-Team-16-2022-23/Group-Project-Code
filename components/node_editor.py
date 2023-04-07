584beb94b703fd713582962e7e3f38839489c789
Ziqiu Jiang <jiangziqiu02@gmail.com>
2023-04-07 22:08:29 +0100

    feat: customized nodes

--- components/node_editor.py
+++ components/node_editor.py
@@ -4,10 +4,17 @@
 """Node editor (Challenging). Handle visualizable process operation"""
 
 from flask import Blueprint, request, render_template, session, redirect
+import json
+import html
+import config
+
+from util.storage_control import list_blobs, upload_blob
 
 bp = Blueprint('node_editor', __name__, url_prefix='/node_editor')
 
 
 @bp.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('node_editor/project.html')
+    datasets = list_blobs(prefix='public', ext_filter=config.ALLOWED_EXTENSIONS)
+    datasets = json.dumps(datasets)
+    return render_template('node_editor/project.html', datasets=datasets)
