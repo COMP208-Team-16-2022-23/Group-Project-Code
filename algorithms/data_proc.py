ef44bacf15ff31cb029f7e735c8c5fac99b37148
sanli84 <H.Lu18@student.liverpool.ac.uk>
2023-04-07 10:56:46 +0100

    add MAD in outlier_handling

--- algorithms/data_proc.py
+++ algorithms/data_proc.py
@@ -68,6 +68,10 @@ def outlier_handling(df, parameters):
     elif detection_method == 'IQR':
         for column_name in parameters['column_selected']:
             df = IQR(df, processing_method, column_name=column_name)
+    elif detection_method == 'MAD':
+        for column_name in parameters['column_selected']:
+            df = MAD(df, processing_method, column_name=column_name)
+
 
     return df
 
@@ -139,6 +143,34 @@ def IQR(df, processing_method, column_name, parameters=None):
 
     return df
 
+
+def MAD(df, processing_method, column_name, parameters=None):
+    """
+            Use MAD method to detect outliers
+            :param df: the pandas dataframe to be processed
+            :param parameters: a dictionary of parameters
+            :processing_method: how to repalce outliers
+            :return: the processed dataframe
+            """
+
+    # get column by name
+    column = df[column_name]
+
+    mean = column.mean()
+    median = column.median()
+
+    #get mad and threshold value
+    mad = np.abs(df[column_name] - median).median()
+    threshold = 3 * mad
+
+    if processing_method == 'set to null':
+        df[column_name] = df[column_name].apply(lambda x: np.nan if abs(x - median) > threshold else x)
+    elif processing_method == 'set to mean':
+        df[column_name] = df[column_name].apply(lambda x: mean if abs(x - median) > threshold else x)
+    elif processing_method == 'set to median':
+        df[column_name] = df[column_name].apply(lambda x: median if abs(x - median) > threshold else x)
+
+    return df
 
 def Z_score(df, parameters):
     """
