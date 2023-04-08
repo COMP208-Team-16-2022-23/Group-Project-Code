96e9322a937c83f2f025e9b9f3a9fc47f9832069
J_LJQ <36041971+Jason-LJQ@users.noreply.github.com>
2023-04-08 22:34:35 +0100

    cookie style test 2

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -1,17 +1,47 @@
-// set cookie function
+// Get the cookie banner element
+var cookieBanner = document.getElementById('cookie-banner');
+
+// Get the close button element
+var closeButton = document.getElementById('close-button');
+
+// Get the body element
+var body = document.getElementsByTagName('body')[0];
+
+// Check if the user has accepted the cookie policy
+if (getCookie('cookie-policy') != 'accepted') {
+    // Show the cookie banner
+    cookieBanner.style.display = 'block';
+
+    // Add the no-scroll class to the body element to block interaction with other parts of the website
+    body.classList.add('no-scroll');
+}
+
+// Add a click event listener to the close button
+closeButton.addEventListener('click', function () {
+    // Set a cookie to indicate that the user has accepted the cookie policy (expires in 365 days)
+    setCookie('cookie-policy', 'accepted', 365);
+
+    // Remove the cookie banner
+    cookieBanner.remove();
+
+    // Remove the no-scroll class from the body element to re-enable scrolling and interaction with other parts of the website
+    body.classList.remove('no-scroll');
+});
+
+// Set a cookie with the given name, value, and expiration days
 function setCookie(name, value, days) {
-    var expires = "";
+    var expires = '';
     if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+        expires = '; expires=' + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + '=' + value + expires + '; path=/';
 }
 
-// get cookie function
+// Get the value of the cookie with the given name
 function getCookie(name) {
-    var nameEQ = name + "=";
+    var nameEQ = name + '=';
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
@@ -19,26 +49,4 @@ function getCookie(name) {
         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
-}
-
-// check if cookie exists
-if (!getCookie('cookie-accepted')) {
-    // show cookie banner
-    var cookieBanner = document.getElementById('cookie-banner');
-    cookieBanner.classList.add('show');
-
-    // block interaction with other parts of the website
-    var body = document.getElementsByTagName('body')[0];
-    body.classList.add('no-scroll');
-
-    // accept cookies button click event
-    var acceptCookiesButton = document.getElementById('accept-cookies');
-    acceptCookiesButton.addEventListener('click', function () {
-        // set cookie and hide cookie banner
-        setCookie('cookie-accepted', 'true', 365);
-        cookieBanner.classList.remove('show');
-
-        // unblock interaction with other parts of the website
-        body.classList.remove('no-scroll');
-    });
 }
