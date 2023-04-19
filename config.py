79cb4b6e4cae7cba7f4d45a0ec73df553b6e2027
J_LJQ <36041971+Jason-LJQ@users.noreply.github.com>
2023-04-19 21:01:01 +0100

    feat: adjust the NOTICE message.

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ LOGIN_REQUIRED = 'Please log in first'
 INTRODUCTION = 'LCDA stands for Low-Code Data Analysis, a revolutionary approach to simplifying data analysis by minimizing the need for coding. With LCDA, you can quickly and easily analyze large datasets without writing complex code. The platform features a user-friendly interface for dragging and dropping data into analyses and visualizing results. LCDA is perfect for data scientists, business analysts, and anyone looking to gain insights from their data more efficiently.'
 
 # Notice in index page
-NOTICE_STATUS = 'True'
+NOTICE_STATUS = 'False'
 NOTICE_TITLE = 'Kind tips:'
 NOTICE_MESSAGE = 'Our platform is being developed and some content may change. We may delete user data for a better user experience, so back up important information regularly. Sorry for any inconvenience and thanks for your understanding. We hope you can enjoy using our platform.'
